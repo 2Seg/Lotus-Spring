9166560de80f0f981ab8790e6e588bf87dd61538
OrianneJoannic <orianne.joannic@isep.fr>
2017-12-12 08:35:48 +0100

    Commit de la vue du Formulaire Professeur

--- src/main/java/com/isep/lotus/controllers/HomeController.java
+++ src/main/java/com/isep/lotus/controllers/HomeController.java
@@ -13,10 +13,16 @@ public class HomeController {
 
     @RequestMapping("/")
     public String index(Model model) {
-        Eleve eleve = new Eleve("Eliott", "de SEGUIER", "eliottdes@gmail.com", true, 8740, "A2", "Académique", "2019");
+        Eleve eleve = new Eleve(1,"Eliott", "de SEGUIER", "eliottdes@gmail.com", true, 8740, "A2", "Académique", "2019");
 //        model.addAllAttributes(Collections.singleton(eleve));
         model.addAttribute(eleve);
         return "home";
+
+    }
+
+    @RequestMapping("/FormulaireProfesseur")
+    public String FormulaireProfesseur() {
+        return "FormulaireProfesseur";
 
     }
 
