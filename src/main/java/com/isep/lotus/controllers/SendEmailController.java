60aa8399dc589a25086c09cabfa6254ed93ec704
quent77np <andrieuquentin8@gmail.com>
2018-01-11 09:20:11 +0100

    gestion envoie de mail multiple

--- src/main/java/com/isep/lotus/controllers/SendEmailController.java
+++ src/main/java/com/isep/lotus/controllers/SendEmailController.java
@@ -14,6 +14,9 @@ package com.isep.lotus.controllers;
         import org.springframework.web.bind.annotation.RequestParam;
         import org.springframework.web.servlet.ModelAndView;
 
+        import java.util.ArrayList;
+        import java.util.List;
+
         import static com.isep.lotus.LotusApplication.getSession;
 
 @Controller
@@ -24,24 +27,29 @@ public class SendEmailController {
     private JavaMailSender mailSender;
 
     @RequestMapping("/message")
-    public ModelAndView doSendEmail(HttpServletRequest request, @RequestParam("email") String adress,@RequestParam("objmessage") String subject,@RequestParam("message") String message, HttpSession httpSession,
+    public ModelAndView doSendEmail(HttpServletRequest request, @RequestParam("email") String[] adress,@RequestParam("objmessage") String subject,@RequestParam("message") String message, HttpSession httpSession,
                               ModelAndView modelAndView) {
         // takes input from e-mail form
 
+        for(int i = 0; i < adress.length; i++)
+        {   // prints debug info
+            System.out.println("To: " + adress[i]);
+
+
+            // creates a simple e-mail object
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setTo(adress[i]);
+            email.setSubject(subject);
+            email.setText(message);
+
+            // sends the e-mail
+            mailSender.send(email);
+        }
 
         // prints debug info
-        System.out.println("To: " + adress);
         System.out.println("Subject: " + subject);
         System.out.println("Message: " + message);
 
-        // creates a simple e-mail object
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo(adress);
-        email.setSubject(subject);
-        email.setText(message);
-
-        // sends the e-mail
-        mailSender.send(email);
 
         // forwards to the view named "Result"
         Session sessionHibernate = getSession();
